a2d0eb5780eb5980bc38bb9ee56b19464de2041d
Eben Vranken <92394428+eben-vranken@users.noreply.github.com>
2024-12-22 15:16:20 +0100

    Added links to home page with intentional twitter with old twitter colors instead of X haha

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 import BlogpostList from "@/components/blog/BlogpostList";
+import Link from "next/link";
+import { GithubLogo, RedditLogo, TwitterLogo } from "@phosphor-icons/react/dist/ssr";
 
 export default async function Home() {
   const filePath = join(process.cwd(), "data", "blogposts.json");
@@ -9,6 +11,22 @@ export default async function Home() {
 
   return (
     <section className="w-full min-h-screen flex items-center flex-col">
+      {/* Links */}
+      <section className="w-full flex gap-x-4 mt-4 text-white">
+        <Link href="https://www.github.com/eben-vranken/exercard" target="_blank" className="flex items-center gap-x-2 rounded bg-[#2b3137] p-2 font-semibold">
+          <GithubLogo size={20} weight="bold" />
+          Github
+        </Link>
+        <Link href="https://www.reddit.com/u/exercard" target="_blank" className="flex items-center gap-x-2 rounded bg-[#FF5700] p-2 font-semibold">
+          <RedditLogo size={20} weight="bold" />
+          Reddit
+        </Link>
+        <Link href="https://www.twitter.com/exercard" target="_blank" className="flex items-center gap-x-2 rounded bg-[#1DA1F2] p-2 font-semibold">
+          <TwitterLogo size={20} weight="bold" />
+          Twitter
+        </Link>
+      </section>
+
       <section className="w-full flex-1 overflow-y-auto">
         <BlogpostList posts={posts} />
       </section>
