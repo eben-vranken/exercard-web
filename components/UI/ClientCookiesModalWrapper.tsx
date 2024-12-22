2eca0aac4d66fdb571c5e7842e18b1420d2af497
Eben Vranken <92394428+eben-vranken@users.noreply.github.com>
2024-12-22 16:03:52 +0100

    Changed import for cookiesModal

--- components/UI/ClientCookiesModalWrapper.tsx
+++ components/UI/ClientCookiesModalWrapper.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
-const CookieConsentModal = dynamic(() => import('@/components/UI/CookiesModal'), { ssr: false });
+const CookieConsentModal = dynamic(() => import('./CookiesModal'), { ssr: false });
 
 export default function ClientWrapper() {
     return (
