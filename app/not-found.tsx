f736df7135bc0a25e22392654e354152432f9b98
Eben Vranken <92394428+eben-vranken@users.noreply.github.com>
2024-12-22 02:53:33 +0100

    Added domain to siteUrl

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound: React.FC = () => {
+    return (
+        <section>
+            <h1>404 - Page Not Found</h1>
+            <p>How did you end up here?</p>
+        </section>
+    );
+}
+
+export default NotFound;
