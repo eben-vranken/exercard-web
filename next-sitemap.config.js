3ccc634657d2162dd37bfe3413586f693eb41358
Eben Vranken <92394428+eben-vranken@users.noreply.github.com>
2024-12-22 02:57:39 +0100

    Added blogposts to sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,21 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.exercard.com',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: '*', disallow: '/admin' },
+    ],
+  },
+  additionalPaths: async (config) => {
+    const fs = require('fs');
+    const path = require('path');
+    const blogPostsPath = path.join(process.cwd(), 'data', 'blogposts.json');
+    const blogPosts = JSON.parse(fs.readFileSync(blogPostsPath, 'utf-8'));
+
+    return blogPosts.map((post) => ({
+      loc: `/blog/${post.slug}`,
+      lastmod: new Date().toISOString(),
+    }));
+  },
 };
