0e91138dc233d0a5df710fbb4740c28972783726
Eben Vranken <92394428+eben-vranken@users.noreply.github.com>
2024-12-23 00:44:44 +0100

    Added blog page with blogpost list

---
+++ app/blog/page.tsx
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import BlogPostList from "@/components/blog/BlogpostList";
+import React from "react";
+
+export default async function Blog() {
+    const filePath = join(process.cwd(), "data", "blogposts.json");
+    const fileContents = readFileSync(filePath, "utf-8");
+    const posts = JSON.parse(fileContents);
+
+    return <section className="w-full flex-1 overflow-y-auto">
+        <BlogPostList posts={posts} />
+    </section>
+};
