684c69752aa52ec533f25aa88234ec0c4e7ac44f
Eben Vranken <92394428+eben-vranken@users.noreply.github.com>
2024-12-23 01:02:49 +0100

    Added interal links and cleaned up github & social links

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 import BlogpostList from "@/components/blog/BlogpostList";
-import Link from "next/link";
-import { GithubLogo, RedditLogo, TwitterLogo } from "@phosphor-icons/react/dist/ssr";
 
 export default async function Home() {
   const filePath = join(process.cwd(), "data", "blogposts.json");
@@ -11,22 +9,6 @@ export default async function Home() {
 
   return (
     <section className="w-full min-h-screen flex items-center flex-col">
-      {/* Links */}
-      <section className="w-full flex gap-x-4 mt-4 text-white">
-        <Link href="https://www.github.com/eben-vranken/exercard" target="_blank" className="flex items-center gap-x-2 rounded bg-[#2b3137] p-2">
-          <GithubLogo size={20} weight="bold" />
-          Github
-        </Link>
-        <Link href="https://www.reddit.com/u/exercard" target="_blank" className="flex items-center gap-x-2 rounded bg-[#FF5700] p-2">
-          <RedditLogo size={20} weight="bold" />
-          Reddit
-        </Link>
-        <Link href="https://www.twitter.com/exercard" target="_blank" className="flex items-center gap-x-2 rounded bg-[#1DA1F2] p-2">
-          <TwitterLogo size={20} weight="bold" />
-          Twitter
-        </Link>
-      </section>
-
       <section className="w-full flex-1 overflow-y-auto">
         <BlogpostList posts={posts} />
       </section>
--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@phosphor-icons/react/dist/ssr";
+import { GithubLogo, RedditLogo, Stack, TwitterLogo } from "@phosphor-icons/react/dist/ssr";
 import Link from "next/link";
 
 const Navbar: React.FC = () => {
@@ -11,6 +11,29 @@ const Navbar: React.FC = () => {
             <p className="text-light mt-2 text-sm">
                 Free & Open-source SRS flashcards with local AI-integration.
             </p>
+
+
+            <section className="flex items-center justify-between w-full">
+                {/* Navigation */}
+                <section className="flex gap-x-4">
+                    <Link href={'/'} className="hover:text-light">Home</Link>
+                    <Link href={'/blog'} className="hover:text-light">Blog</Link>
+                </section>
+
+                {/* Github & Socials */}
+                <section className="flex gap-x-4 mt-4 text-white">
+                    <Link href="https://www.github.com/eben-vranken/exercard" target="_blank" className="flex items-center gap-x-2 rounded bg-[#2b3137] p-2">
+                        <GithubLogo size={20} weight="bold" />
+
+                    </Link>
+                    <Link href="https://www.reddit.com/u/exercard" target="_blank" className="flex items-center gap-x-2 rounded bg-[#FF5700] p-2">
+                        <RedditLogo size={20} weight="bold" />
+                    </Link>
+                    <Link href="https://www.twitter.com/exercard" target="_blank" className="flex items-center gap-x-2 rounded bg-[#1DA1F2] p-2">
+                        <TwitterLogo size={20} weight="bold" />
+                    </Link>
+                </section>
+            </section>
         </nav>
     )
 };
